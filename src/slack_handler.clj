32628e8375478ec17be11a1c867057a764322590
jonathanmourtada <jonathan.mourtada@divid.se>
2017-02-25 12:45:55 +0100

    working slack api

--- src/slack_handler.clj
+++ src/slack_handler.clj
@@ -1,7 +1,8 @@
 (ns slack-handler
     (:require
         [ring.middleware.json :refer [wrap-json-response]]
-        [ring.util.response :refer [response]]))
+        [ring.util.response :refer [response]]
+        [ring.middleware.params :refer [wrap-params]]))
 
 (defn format-dishes [dishes]
     (reduce (fn [acc dish]
@@ -22,11 +23,14 @@
             restaurants))
 
 (defn handler [request]
-    (println request)
-    (let [html-data (parser/get-html-data)
-          restaurants (parser/get-restaurants html-data)]
+    (println )
+    (let [search-word (get (:form-params request) "text")
+          html-data (parser/get-html-data)
+          restaurants (parser/get-restaurants html-data)
+          filtered-restaurants (filter (partial parser/has-dish-filter search-word) restaurants)]
         (response {:response_type "in_channel"
-                   :text (format-slack-message restaurants)})))
+                   :text (format-slack-message filtered-restaurants)})))
 
 (def app
-    (wrap-json-response handler))
+    (wrap-params
+        (wrap-json-response handler)))
